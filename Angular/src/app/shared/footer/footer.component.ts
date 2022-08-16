0255341c59b221970a967c9c3274eb7cabd8ffe1
adamgate <applegateadaml@gmail.com>
2022-08-16 12:54:45 -0600

    Fixed footer to always have current date

--- Angular/src/app/shared/footer/footer.component.ts
+++ Angular/src/app/shared/footer/footer.component.ts
@@ -1,13 +1,18 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers: [DatePipe]
 })
 export class FooterComponent implements OnInit {
+  currentYear: string;
 
-  constructor() { }
+  constructor(private datePipe: DatePipe) {
+      this.currentYear = this.datePipe.transform(new Date(), 'yyyy');
+   }
 
   ngOnInit(): void {
   }

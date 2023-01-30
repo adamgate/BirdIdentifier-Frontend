b242d591849d5e1d97ab6119342b0497203b91ac
adamgate <applegateadaml@gmail.com>
2023-01-30 14:22:39 -0700

    Overhauled bird identification functionality

--- CommandLine/CommandLine/Menu.cs
+++ CommandLine/CommandLine/Menu.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
 
 namespace CommandLine
 {
@@ -139,25 +143,56 @@ namespace CommandLine
             return filePath;
         }
 
-        //private FileInfo LoadImage(string imagePath)
-        //{
-        //    FileInfo image;
+        private MultipartFormDataContent LoadAndConvertImage(string imagePath)
+        {
+            Console.Write("Loading image....  ");
+            var image = File.ReadAllBytes(imagePath);
+            Console.WriteLine("Done.");
 
-        //    return image;
-        //}
+            Console.Write("Processing image...  ");
+            var content = new MultipartFormDataContent();
+            var byteImage = new ByteArrayContent(image);
 
-        private async Task IdentifyBird()
+            content.Add(byteImage, "image");
+            Console.WriteLine("Done.");
+
+            return content;
+        }
+
+        private void IdentifyBird()
         {
             //get image path
             string imagePath = GetImagePath();
 
-            //load image
-
-            //convert image to proper format
+            //load and convert image to proper format
+            var payload = LoadAndConvertImage(imagePath);
 
             //create request and send it in
+            HttpResponseMessage response;
+            try
+            {
+                Console.Write("Sending image to server for analysis...  ");
+                response = client.PostAsync("/images", payload).Result;
+                Console.WriteLine("Done.");
 
-            //print out the information for the user
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException re)
+            {
+                Console.WriteLine($"There was an issue analyzing the image: \"{re.Message}\"");
+                return;
+            }
+            catch (AggregateException ae)
+            {
+                Console.WriteLine($"Couldn't connect to the backend. Error: \"{ae.Message}\"");
+                return;
+            }
+
+            if (response.Content != null)
+            {
+                Prediction prediction = JsonConvert.DeserializeObject<Prediction>(response.Content.ToString());
+                Console.WriteLine(prediction.ToString());
+            }
         }
     }
 }

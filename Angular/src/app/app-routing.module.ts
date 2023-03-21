90d342c10487259b606b5c76c1e7aefcc7835b60
adamgate <applegateadaml@gmail.com>
2023-03-21 11:42:33 -0600

    Removed unnecessary components

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { WhatAreYouComponent } from './pages/what-are-you/what-are-you.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'about', component: AboutComponent },
   { path: 'what-are-you', component: WhatAreYouComponent },
 ];
 
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,11 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ImageUploadComponent } from './pages/home/image-upload/image-upload.component';
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { WhatAreYouComponent } from './pages/what-are-you/what-are-you.component';
 import { PredictionItemComponent } from './pages/predictions/prediction-item/prediction-item.component';
-import { PredictionListComponent } from './pages/predictions/prediction-list/prediction-list.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +17,9 @@ import { PredictionListComponent } from './pages/predictions/prediction-list/pre
     HeaderComponent,
     FooterComponent,
     ImageUploadComponent,
-    AboutComponent,
     HomeComponent,
     WhatAreYouComponent,
-    PredictionItemComponent,
-    PredictionListComponent
+    PredictionItemComponent
   ],
   imports: [
     BrowserModule,
